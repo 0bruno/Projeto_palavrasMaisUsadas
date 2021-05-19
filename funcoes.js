9fcef1b45d5ba74c559e1ed39585de4fc148edcf
Bruno Ribeiro <brunobsribeiro@gmail.com>
2021-05-18 21:24:12 -0300

    feat: creat function to readFile

--- funcoes.js
+++ funcoes.js
@@ -1,6 +1,12 @@
+const fs = require('fs');
+const path = require('path');
+
 //criar funcao responsavel por ler um diretorio
-function lerDiretorio(caminho) {}
-//exportando funcao
+function lerDiretorio(caminho) {
+  let arquivos = fs.readdirSync(caminho);
+  return arquivos.map((arquivos) => path.join(caminho, arquivos));
+}
+
 module.exports = {
   lerDiretorio,
 };
--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-//exportando funcoes
+const path = require('path');
+const fn = require('./funcoes');
+const caminho = path.join(__dirname, 'legendas');
+
+const arquivos = fn.lerDiretorio(caminho);
+console.log(arquivos);
