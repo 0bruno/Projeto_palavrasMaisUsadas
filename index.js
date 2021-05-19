afa18980ef11c141856ecb88736716484e1038ff
Bruno Ribeiro <brunobsribeiro@gmail.com>
2021-05-19 19:41:12 -0300

    feat: filter all lines

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ fn.lerDiretorio(caminho)
   //filtandro terminados com srt
   .then((arquivos) => fn.arquivosTerminadosCom(arquivos, '.srt'))
   .then((arquivosSRT) => fn.lerArquivos(arquivosSRT))
+  //juntando todos os conteudos com join no array
+  .then((conteudos) => conteudos.join('\n'))
+  //quebrando todas as linhas de todos os arquivos
+  .then((todosConteudos) => todosConteudos.split('\n'))
   .then(console.log)
   .catch((err) => console.log(err));
 console.log('Executou primeiro');
