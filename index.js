f810e3480ba0eee6ddc99dea5ea6bdbc903764db
Bruno Ribeiro <brunobsribeiro@gmail.com>
2021-05-19 18:56:57 -0300

    refactor: filter files end .srt

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const fn = require('./funcoes');
 const caminho = path.join(__dirname, 'legendas');
 
 fn.lerDiretorio(caminho)
+  //filtandro terminados com srt
   .then((arquivos) => fn.arquivosTerminadosCom(arquivos, '.srt'))
   .then(console.log)
   .catch((err) => console.log(err));
