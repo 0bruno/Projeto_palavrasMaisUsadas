9f4db1d00c00424a04f51d62c3baee300d233e57
Bruno <brunobsribeiro@gmail.com>
2021-05-25 20:34:44 -0300

    feat: fazendo split ' ' para separar por palavras

--- index.js
+++ index.js
@@ -19,14 +19,18 @@ const simbolos = [
   '♪',
 ];
 
+const mesclarConteudos = (conteudos) => conteudos.join('\n');
+const separarPorLinhas = (todosConteudos) => todosConteudos.split('\n');
+const separarPorPalavras = (todosConteudos) => todosConteudos.split(' ');
+
 fn.lerDiretorio(caminho)
   //filtandro terminados com srt.
   .then((arquivos) => fn.arquivosTerminadosCom(arquivos, '.srt'))
   .then((arquivosSRT) => fn.lerArquivos(arquivosSRT))
   //juntando todos os conteudos com join no array.
-  .then((conteudos) => conteudos.join('\n'))
+  .then(mesclarConteudos)
   //quebrando todas as linhas de todos os arquivos separando o texto em varias linhas.
-  .then((todosConteudos) => todosConteudos.split('\n'))
+  .then(separarPorLinhas)
   //removendo linhas vazias com trim().
   .then((linhasVazias) => fn.removeSeVazio(linhasVazias))
   //removendo se tiver algum padrao
@@ -35,6 +39,10 @@ fn.lerDiretorio(caminho)
   .then((removeNumeros) => fn.removeNumero(removeNumeros))
   //removendo Caracteres
   .then((removeCaracteres) => fn.removeCaracteres(simbolos)(removeCaracteres))
+  //mesclando conteudos denovo
+  .then(mesclarConteudos)
+  //separando por palavras com split em cima de um espaco em branco
+  .then(separarPorPalavras)
 
   .then(console.log)
   .catch((err) => console.log(err));
